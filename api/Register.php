a38793c50be3bec1944738a48874c436f1114f8b
francomolina333 <francomolina333@gmail.com>
2022-05-28 22:26:51 -0400

    Update Register.php

--- api/Register.php
+++ api/Register.php
@@ -1,11 +1,10 @@
 <?php
 
 	$inData = getRequestInfo();
-	$FirstName = "";
-	$LastName = "";
-	$Login= "";
-	$Password = "";
-	$id = 0;
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
 	
 
 	$conn = new mysqli("localhost", "TheBeast4", "WeLoveCOP4331", "COP4331"); 	
@@ -15,27 +14,27 @@
 	}
 	else
 	{
-		echo "Connection succesful";
-	}
-		
-        $sql = "INSERT INTO Users(FirstName, LastName, Login, Password)
-		VALUES ('$inData["FirstName"]', '$inData["LastName"]', '$inData["Login"]', '$inData["Password"]')";
 
-		if ($conn->query($sql))
-		{
-			$row = $result->fetch_assoc();
-			$FirstName = $row['FirstName'];
-			$LastName = $row['LastName'];
-			$id = $row['ID'];
+		$stmt = $conn->prepare("INSERT into Users (firstName,lastName,login,password) VALUES(?,?,?,?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+		$stmt->execute();
+		$stmt->close();
+		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();
 
-			returnWithInfo($FirstName, $LastName, $id);
-  		} 	
-		else 
+		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithError("Account Creation Failed");
-  		}
-		  
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+		}
+		else
+		{
+		returnWithError("Account created?"); // Account made, no error
+		}
+		$stmt->close();
 		$conn->close();
+	}
 	
 	function getRequestInfo()
 	{
@@ -50,13 +49,13 @@
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		$retValue = '{"id":0,"FirstName":"","LastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
 	function returnWithInfo( $firstName, $lastName, $id )
 	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		$retValue = '{"id":' . $id . ',"FirstName":"' . $firstName . '","LastName":"' . $lastName . '","error":"Account Created"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
