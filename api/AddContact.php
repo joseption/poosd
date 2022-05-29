b2c3bb7944b4ccd78c91791e8cce3b7bff7e8a8e
francomolina333 <francomolina333@gmail.com>
2022-05-28 23:48:59 -0400

    Update AddContact.php

--- api/AddContact.php
+++ api/AddContact.php
@@ -1,9 +1,19 @@
 <?php
+
+	//
+	//
+	//
+	//
+	//
 	$inData = getRequestInfo();
+
+	$userid = $inData["userId"];
+	$NewContact = $inData["contact"];
+
 	
-	$name = $inData["name"];
-	$phone = $inData["phone"];
-	$email = $inData["email"];
+	$name = $NewContact["name"];
+	$phone = $NewContact["phone"];
+	$email = $NewContact["email"];
 
 	$conn = new mysqli("localhost", "TheBeast4", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
@@ -12,12 +22,26 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email) VALUES(?,?,?)");
-		$stmt->bind_param("sss", $name, $phone, $email);
+		$stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email, UserID) VALUES(?,?,?,?)");
+		$stmt->bind_param("sssi", $name, $phone, $email, $userid);
 		$stmt->execute();
 		$stmt->close();
+		
+		$stmt = $conn->prepare("SELECT Name, Phone, Email FROM Contacts WHERE userid=?");
+		$stmt->bind_param("s", $userid);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['Name'], $row['Phone'], $row['Email'] );
+		}
+		else
+		{
+		returnWithError("Contact Creation Failed"); // Account not verified
+		}
+		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -31,6 +55,12 @@
 		echo $obj;
 	}
 	
+	function returnWithInfo( $name, $phone, $email )
+	{
+		$retValue = '{"Name":' . $name . ',"phone":"' . $phone . '","email":"' . $email . '","error":"Contact Added"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error":"' . $err . '"}';
